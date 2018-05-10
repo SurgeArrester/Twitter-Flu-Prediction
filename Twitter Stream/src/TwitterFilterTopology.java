eec23b1d47c163fed477d250d673d399d781e608
SurgeArrester <cameron.h@rgreaves.me.uk>
2018-05-10 16:36:50 +0100

    Remove Twitter key

--- Twitter Stream/src/TwitterFilterTopology.java
+++ Twitter Stream/src/TwitterFilterTopology.java
@@ -23,43 +23,43 @@ import twitter4j.conf.*;
  */
 public class TwitterFilterTopology {
 
-    private static String consumerKey = "w3EU8GdlaoBRKpV1Fsv6gz4MG";
-    private static String consumerSecret = "3dMfugvWRYX6olkgU7afSUPlMckJ9feuT5KMerub4TCJ9c4dwN";
-    private static String accessToken = "932643962959093760-vcx8nb5umxBo8XnBlBEJuGfHOpycCbF";
-    private static String accessTokenSecret = "d7AxAhi1zrPjpeVABssIY0tswHXxr5gMUaaNJvRuRxsmA";
-    
+    private static String consumerKey = "";
+    private static String consumerSecret = "";
+    private static String accessToken = "";
+    private static String accessTokenSecret = "";
+
     private static List<String> readCsvToArray(String fileLocation) throws IOException {
         String city = "";
         List<String> cityList = new ArrayList<String>();
         BufferedReader br = new BufferedReader(new FileReader(fileLocation));
         while ((city = br.readLine()) != null) {
         	cityList.add(city);
-        }   
+        }
         return cityList;
     }
 
-    public static void main(String[] args) throws Exception {                
+    public static void main(String[] args) throws Exception {
         ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
         configurationBuilder.setOAuthConsumerKey(consumerKey)
                 .setOAuthConsumerSecret(consumerSecret)
                 .setOAuthAccessToken(accessToken)
                 .setOAuthAccessTokenSecret(accessTokenSecret);
-        
+
         List<String> cityList = readCsvToArray("./static/simplemaps-worldcities-basic-unfiltered.csv");
 
         TwitterStream twitterStream = new TwitterStreamFactory(configurationBuilder.build()).getInstance();
-        
+
         TopologyBuilder builder = new TopologyBuilder();
 
         FilterQuery tweetFilterQuery = new FilterQuery();
-        
-      	tweetFilterQuery.track(new String[]{"going to", 
-      										"on my way to", 
-      										"travelling to", 
-      										"popping to", 
-      										"vacationing to", 
-      										"driving to", 
-      										"flying to", 
+
+      	tweetFilterQuery.track(new String[]{"going to",
+      										"on my way to",
+      										"travelling to",
+      										"popping to",
+      										"vacationing to",
+      										"driving to",
+      										"flying to",
       										"cycling to",
       										"on the train to",
       										"sailing to",
@@ -71,15 +71,15 @@ public class TwitterFilterTopology {
       										"off to",
       										"popping to",
       										"away to"});
-      	
+
       	// See https://github.com/kantega/storm-twitter-workshop/wiki/Basic-Twitter-stream-reading-using-Twitter4j
-      	
+
         TwitterSpout spout = new TwitterSpout(consumerKey, consumerSecret, accessToken, accessTokenSecret, tweetFilterQuery);
         discardNonPlace discardNonPlace = new discardNonPlace();
         CheckFluPyBolt checkFluPyBolt = new CheckFluPyBolt();
         destinationEstimator destinationEstimator = new destinationEstimator(cityList);
         FileWriterBolt fileWriterBolt = new FileWriterBolt("MyTweets.txt");
-        
+
         builder.setSpout("TwitterSpout", spout);				// Initial spout to get data from twitter
         builder.setBolt("discardNonPlace", discardNonPlace).shuffleGrouping("TwitterSpout");	// Remove all tweets that don't have a place associated
         builder.setBolt("CheckFluPyBolt", checkFluPyBolt).shuffleGrouping("discardNonPlace");
